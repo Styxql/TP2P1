43d6a1f262167218cfa92a49d50c0bda0b1beb99
Quentin Laval <quentin.laval@etu.univ-smb.fr>
2024-02-05 11:47:20 +0100

    s

---
+++ TP2P1Tests/Controllers/SeriesControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP2P1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TP2P1.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP2P1.Controllers.Tests
+{
+    
+    
+
+    [TestClass()]
+    public class SeriesControllerTests
+    {
+        private SeriesController controller;
+
+        public SeriesControllerTests()
+        {
+            var builder = new DbContextOptionsBuilder<SeriesDBContext>().UseNpgsql("Server = localhost; port=5432;Database=SeriesDB; uid=postgres;password=postgres;");
+            SeriesDBContext context = new SeriesDBContext(builder.Options);
+            controller = new SeriesController(context);
+
+        }
+
+        [TestMethod()]
+        public  void GetSeriesTest()
+        {
+            Task<ActionResult<IEnumerable<Serie>>> expectedSeries =  controller.GetSeries();
+
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSeries, actualSeries);
+
+        }
+}
